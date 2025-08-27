a0fc1e5097c74786ad683235ad639d755de53494
Diogo Negreiros <diogomanoel495@gmail.com>
2025-08-27 18:26:00 -0300

    Criação da classe Receipt e resolução da classe TaxCalculator usando SRP

--- src/main/java/br/com/mariojp/solid/srp/TaxCalculator.java
+++ src/main/java/br/com/mariojp/solid/srp/TaxCalculator.java
@@ -1,4 +1,15 @@
 package br.com.mariojp.solid.srp;
 
 public class TaxCalculator {
+    private final double taxRate;
+
+    public TaxCalculator(double taxRate) {
+        String taxRateProperty = System.getProperty("tax.rate");
+        this.taxRate = taxRate;
+    }
+
+    public double calculateTaxRate(double subtotal) {
+        return subtotal * this.taxRate;
+    }
+
 }
